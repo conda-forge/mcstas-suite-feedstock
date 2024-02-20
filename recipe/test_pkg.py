f002f0143a077956d062f69a3237ce13f81f301d
Peter Willendrup <pkwi@fysik.dtu.dk>
2024-02-20 15:06:07 +0100

    Catchall-commit including changes proposed in https://github.com/conda-forge/mcstas-suite-feedstock/pull/12 / https://github.com/conda-forge/mcstas-suite-feedstock/pull/13 - And most of the earlier review comments taken into account

--- recipe/test_pkg.py
+++ recipe/test_pkg.py
@@ -26,6 +26,7 @@ def work_in_tmpdir():
         with tempfile.TemporaryDirectory() as tmpdir:
             os.chdir(tmpdir)
             yield
+            os.chdir(the_cwd)
     finally:
         os.chdir(the_cwd)
 
@@ -42,7 +43,10 @@ def launch( cmd, **kwargs ):
     return res
 
 def query_mcrun_showcfgdir( cfgdirname='resourcedir', must_exist = True ):
-    cmd = f'mcrun --showcfg {cfgdirname}'
+    if platform.system().lower()=='windows':
+        cmd = f'mcrun.bat --showcfg {cfgdirname}'
+    else:
+        cmd = f'mcrun --showcfg {cfgdirname}'
     res = launch(cmd, capture_output = True, text = True )
     p = pathlib.Path(res.stdout.strip())
     if must_exist and not p.is_dir():
@@ -73,7 +77,10 @@ def run_instrument_file( instrumentfile, parameters = '' ):
         shutil.copy(f,str(AbsPath('.')))
         launch( f'mcstas {f.name}' )
         pars = '' if not parameters else ' %s'%parameters
-        launch( f'mcrun -c {f.name}{pars}' )
+        if platform.system().lower()=='windows':
+            launch( f'mcrun.bat -c {f.name}{pars}' )
+        else:
+            launch( f'mcrun -c {f.name}{pars}' )
 
 def common_tests_for_core_and_mcstas_pkgs( take_instr_file_from_src ):
     mcrun_resourcedir = query_mcrun_showcfgdir( 'resourcedir', must_exist = True )
@@ -82,25 +89,42 @@ def common_tests_for_core_and_mcstas_pkgs( take_instr_file_from_src ):
     assert mcrun_resourcedir == conda_prefix_dir / 'share' / 'mcstas' / 'resources'
     assert mcrun_bindir == conda_prefix_dir / 'bin'
 
-    ensure_files_are_installed( [
-        'bin/mcstas',
-        'bin/mcrun',
-        'bin/mcgui',
-        'share/mcstas/tools/Python/mccodelib/__init__.py',
-        'share/mcstas/resources/examples/BNL_H8.instr',
-    ] )
-    ensure_basic_commands_run( [
-        'mcstas --help',
-        'mcstas --version',
-        'mcrun --showcfg bindir',
-        'mcrun --showcfg resourcedir',
-        'mcrun --showcfg libdir',
-    ] )
+    if platform.system().lower()=='windows':
+        ensure_files_are_installed( [
+            'bin/mcstas.exe',
+            'bin/mcrun.bat',
+            'bin/mcgui.bat',
+            'share/mcstas/tools/Python/mccodelib/__init__.py',
+            'share/mcstas/resources/examples/BNL/BNL_H8/BNL_H8.instr',
+        ] )
+
+        ensure_basic_commands_run( [
+            'mcstas.exe --help',
+            'mcstas.exe --version',
+            'mcrun.bat --showcfg bindir',
+            'mcrun.bat --showcfg resourcedir',
+            'mcrun.bat --showcfg libdir',
+        ] )
+    else:
+        ensure_files_are_installed( [
+            'bin/mcstas',
+            'bin/mcrun',
+            'bin/mcgui',
+            'share/mcstas/tools/Python/mccodelib/__init__.py',
+            'share/mcstas/resources/examples/BNL/BNL_H8/BNL_H8.instr',
+        ] )
+        ensure_basic_commands_run( [
+            'mcstas --help',
+            'mcstas --version',
+            'mcrun --showcfg bindir',
+            'mcrun --showcfg resourcedir',
+            'mcrun --showcfg libdir',
+        ] )
 
 
     instrprefix = 'src/mcstas-comps' if take_instr_file_from_src else 'share/mcstas/resources'
-    run_instrument_file( f'{instrprefix}/examples/BNL_H8.instr', 'lambda=2.36 -s1000 -n1e5')
-    #FIXME: Fails runtime, enable once we find a cure: run_instrument_file( f'{instrprefix}/examples/Union_manual_example.instr', '-s1000 -n1e5')
+    run_instrument_file( f'{instrprefix}/examples/BNL/BNL_H8/BNL_H8.instr', 'lambda=2.36 -s1000 -n1e5')
+    #FIXME: Fails runtime, enable once we find a cure: run_instrument_file( f'{instrprefix}/examples/Union_demos/Union_manual_example/Union_manual_example.instr', '-s1000 -n1e5')
 
 _sample_data_files = ['share/mcstas/resources/data/Be.laz']
 
@@ -163,21 +187,21 @@ def tests_for_pkg_mcstas():
         return
 
     #MPI test (disabled for now):
-    #run_instrument_file( 'share/mcstas/resources/examples/BNL_H8.instr', 'lambda=2.36 -s1000 -n1e5 --mpi=2')
+    #run_instrument_file( 'share/mcstas/resources/examples/BNL/BNL_H8/BNL_H8.instr', 'lambda=2.36 -s1000 -n1e5 --mpi=2')
 
     #MCPL test:
-    run_instrument_file( 'share/mcstas/resources/examples/Test_MCPL_input.instr', '-s1000 repeat=1')
-    run_instrument_file( 'share/mcstas/resources/examples/Test_MCPL_output.instr', '-s1000 Ncount=1e3')
+    run_instrument_file( 'share/mcstas/resources/examples/Tests_MCPL_etc/Test_MCPL_input/Test_MCPL_input.instr', '-s1000 repeat=1')
+    run_instrument_file( 'share/mcstas/resources/examples/Tests_MCPL_etc/Test_MCPL_output/Test_MCPL_output.instr', '-s1000 Ncount=1e3')
 
     #NCrystal test with NCrystal-shipped data:
-    run_instrument_file( 'share/mcstas/resources/examples/NCrystal_example.instr','sample_cfg=Al_sg225.ncmat -s1000 -n1e5' )
+    run_instrument_file( 'share/mcstas/resources/examples/NCrystal/NCrystal_example/NCrystal_example.instr','sample_cfg=Al_sg225.ncmat -s1000 -n1e5' )
 
     #NCrystal test with data from mcstas-data package:
     #  Note: skipped since  .laz/.lau/.nxs support no longer enabled by default in NCrystal (requires custom plugin):
-    #  run_instrument_file( 'share/mcstas/resources/examples/NCrystal_example.instr','sample_cfg=%s -s1000 -n1e5'%shlex.quote(f_Be_laz) )
+    #  run_instrument_file( 'share/mcstas/resources/examples/NCrystal/NCrystal_example/NCrystal_example.instr','sample_cfg=%s -s1000 -n1e5'%shlex.quote(f_Be_laz) )
 
     #NCrystal+Union test:
-    run_instrument_file( 'share/mcstas/resources/examples/Union_NCrystal_example.instr','-s1000 -n1e5' )
+    run_instrument_file( 'share/mcstas/resources/examples/NCrystal/Union_NCrystal_example/Union_NCrystal_example.instr','-s1000 -n1e5' )
 
 
 if __name__=='__main__':

09a0dbf20251387ddd2cca112c54b3aec563aa75
Peter Willendrup <pkwi@fysik.dtu.dk>
2025-08-06 13:10:12 +0200

    Prepare for mpi test on Linux without rsh/ssh available

--- recipe/test_pkg.py
+++ recipe/test_pkg.py
@@ -190,12 +190,16 @@ def tests_for_pkg_mcstas():
     if not f_Be_laz.exists():
         raise SystemExit('Did not find Be.laz in expected location')
 
-    if 'ppc' in platform.processor().lower():
-        print('ppc processor detected - skipping NCrystal/MCPL/mpi tests')
-        return
+    if 'linux' in platform.system().lower():
+        print('linux detected - reconfiguring for openmpi without ssh/rsh')
+        os.environ["MCSTAS_MPIRUN_OVERRIDE"] = "mpirun --mca btl self,vader,tcp --oversubscribe"
 
-    #MPI test (disabled for now):
-    #run_instrument_file( 'share/mcstas/resources/examples/BNL/BNL_H8/BNL_H8.instr', 'lambda=2.36 -s1000 -n1e5 --mpi=2')
+    #MPI test
+    run_instrument_file( 'share/mcstas/resources/examples/BNL/BNL_H8/BNL_H8.instr', 'lambda=2.36 -s1000 -n1e5 --mpi=2 --verbose')
+
+    if 'ppc' in platform.processor().lower():
+        print('ppc processor detected - skipping NCrystal/MCPL tests')
+        return
 
     #MCPL test:
     run_instrument_file( 'share/mcstas/resources/examples/Tests_MCPL_etc/Test_MCPL_input/Test_MCPL_input.instr', '-s1000 repeat=1')
